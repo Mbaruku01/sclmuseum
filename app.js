1aa1921c5d3dfdc475812ea44b0e69474d423ce4
Mbaruku01 <hamismbaruku52@gmail.com>
2026-03-15 07:28:30 +0300

    test 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 pannellum.viewer('panorama', {
     "type": "equirectangular",
-    "panorama": "PXL_20260315_040404775.PHOTOSPHERE.jpg", 
+    "panorama": "PXL_20260315_042428263.PHOTOSPHERE.jpg", 
     "autoLoad": true,
     "autoRotate": -2, // Slowly rotates the room for a museum feel
     "title": "SCL Museum Interior",
